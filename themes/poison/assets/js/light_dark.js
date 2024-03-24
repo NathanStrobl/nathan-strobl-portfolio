98d22401e03c7b5e2a64a242c617e5ee707e5593
NathanStrobl <111808514+NathanStrobl@users.noreply.github.com>
2024-03-24 15:17:38 -0400

    Added HTTP to Technologies/Frameworks section on resumé.

--- themes/poison/assets/js/light_dark.js
+++
@@ -1,3 +0,0 @@
-window.addEventListener("DOMContentLoaded", () => {
-    window.matchMedia("(prefers-color-scheme: dark)").matches ? document.body.classList.add("dark-theme") : document.body.classList.remove("dark-theme");
-})
